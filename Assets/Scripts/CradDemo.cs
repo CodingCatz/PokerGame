405fecd99bc035047e84c611e82115acd0e97e93
Catz (CodingCat) <103620558+CodingCatz@users.noreply.github.com>
2026-08-12 21:35:28 +0800

    Class01:物件導向基本觀念&命名空間(資料結構)

---
+++ Assets/Scripts/CradDemo.cs
@@ -0,0 +1,22 @@
+﻿using UnityEngine;//using 使用XXXX命名空間，同等於資料夾的概念
+using PokerGame.Core;
+
+//繼承於UNITY核心腳本：MonoBehaviour，能在前台運作
+public class CradDemo : MonoBehaviour
+{
+    //宣告類型 名稱 = 新建 實體();
+    public PlayingCard testCard = new PlayingCard();
+
+    //按下PLAY後執行一次，初始化
+    void Start()
+    {
+        //利用除錯訊息視窗印出指定內容
+        Debug.Log(testCard.suit + testCard.rank);
+    }
+
+    //遊戲每一FPS執行一次，偵測操作/刷新內容
+    void Update()
+    {
+        
+    }
+}
