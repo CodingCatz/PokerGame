34238aa513fff62f11c8154ee0beea1ba249d235
Catz (CodingCat) <103620558+CodingCatz@users.noreply.github.com>
2026-08-24 20:30:22 +0800

    Class06:[副系統]下注功能關於狀態&錢包部分

---
+++ Assets/Scripts/Betting/RoundOutcome.cs
@@ -0,0 +1,24 @@
+﻿using UnityEngine;
+
+namespace PokerGame.Betting
+{
+    /// <summary>
+    /// [列舉]每一回合相對於玩家來說的結果
+    /// </summary>
+
+    public enum RoundOutcome
+    {
+        /// <summary>
+        /// 玩家勝
+        /// </summary>
+        Win,
+        /// <summary>
+        /// 玩家敗
+        /// </summary>
+        Lose,
+        /// <summary>
+        /// 平手退注
+        /// </summary>
+        Push
+    }
+}
