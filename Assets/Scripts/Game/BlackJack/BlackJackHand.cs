0f3b4e3c9db9da1e6972da2cfcc61245d8036f10
Catz (CodingCat) <103620558+CodingCatz@users.noreply.github.com>
2026-08-28 19:53:38 +0800

    Class08:21點基礎建設(核心系統沿用)

---
+++ Assets/Scripts/Game/BlackJack/BlackJackHand.cs
@@ -0,0 +1,36 @@
+﻿using PokerGame.Core;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace PokerGame.Game.BlackJack
+{
+
+    public class BlackJackHand : MonoBehaviour
+    {
+        #region 欄位
+        /// <summary>
+        /// 手牌存放處(清單物件)
+        /// </summary>
+        private readonly List<PlayingCard> _cards = new List<PlayingCard>();
+        #endregion 欄位
+
+        #region 公開方法
+        /// <summary>
+        /// 將撲克牌加入手牌
+        /// </summary>
+        /// <param name="card">撲克牌資料</param>
+        public void Add(PlayingCard card)
+        {
+            _cards.Add(card);
+        }
+        /// <summary>
+        /// 清空手牌資料(起新局)
+        /// </summary>
+        public void Clear()
+        {
+            _cards.Clear();
+        }
+        #endregion 公開方法
+    }
+}
+
--- Assets/Scripts/Game/HighCard/HighCardGameController.cs
+++ Assets/Scripts/Game/HighCard/HighCardGameController.cs
@@ -1,12 +1,24 @@
 ﻿using PokerGame.Betting;
 using PokerGame.Core;
 using PokerGame.Game;
+using System;
+using TMPro;
 using UnityEngine;
 
 namespace PokerGame.Game.HighCard
 {
+    /// <summary>
+    /// 比大小的遊戲流程主控程式
+    /// </summary>
     public class HighCardGameController : MonoBehaviour
     {
+        #region UI元件
+        [SerializeField]
+        private TMP_Text _betLabel;
+        [SerializeField]
+        private TMP_Text _blanceLabel;
+        #endregion UI元件
+
         #region 欄位
         [SerializeField]
         private Dealer _dealer;
@@ -15,6 +27,7 @@ namespace PokerGame.Game.HighCard
         private Transform _playerHand;
         [SerializeField]
         private Transform _dealerHand;
+        private int _betAmount = 10;
         #endregion 欄位
 
         #region 私有欄位
@@ -22,17 +35,49 @@ namespace PokerGame.Game.HighCard
         /// 該遊戲專屬持有的遊戲規則書
         /// </summary>
         private readonly HighCardRules _rules = new HighCardRules();
+        /// <summary>
+        /// 取得玩家錢包數值捷徑
+        /// </summary>
+        private int _blance => _session.Betting.Balance;
         #endregion 私有欄位
 
         #region 生命週期
         void Start()
         {
             _session = TableSession.Instance;
-            PlayRound();
+            UpdateBetUI();
+            UpdateBlanceUI();
         }
         #endregion 生命週期
 
         #region 公開方法
+        /// <summary>
+        /// [UI按鈕]加注
+        /// </summary>
+        public void AddBet()
+        {
+            _betAmount = Math.Min(_blance, _betAmount + 10);
+            UpdateBetUI();
+        }
+        /// <summary>
+        /// [UI按鈕]減注
+        /// </summary>
+        public void ReduceBet()
+        {
+            _betAmount = Math.Max(10, _betAmount - 10);
+            UpdateBetUI();
+        }
+        /// <summary>
+        /// [UI按鈕]確認下注金
+        /// </summary>
+        public void ApplyBetting()
+        {
+            _session.Betting.TryPlaceBet(_betAmount);
+            //UpdateBlanceUI();
+            //原本的流程節奏該要先執行一次：看到餘額減少
+            _dealer.CollectAll();
+        }
+
         /// <summary>
         /// 遊玩回合
         /// </summary>
@@ -45,10 +90,28 @@ namespace PokerGame.Game.HighCard
             PlayingCard dealerCard = _dealer.DealTo(_dealerHand);
             //用規則書取得結果
             RoundResult result = _rules.Resolve(playCard, dealerCard);
-
-            Debug.Log(result.Reason);
+            int returnChips = _session.Betting.Settle(result);
+            UpdateBlanceUI();
+            //Debug.Log($"{result.Reason} 獲得 {returnChips} 籌碼。");
         }
         #endregion 公開方法
+
+        #region 私有方法
+        /// <summary>
+        /// 更新下注金的UI
+        /// </summary>
+        private void UpdateBetUI()
+        {
+            _betLabel.text = _betAmount.ToString();
+        }
+        /// <summary>
+        /// 更新玩家籌碼餘額的UI
+        /// </summary>
+        private void UpdateBlanceUI()
+        {
+            _blanceLabel.text = _blance.ToString();
+        }
+        #endregion 私有方法
     }
 }
 
