23c1a3d08424302638ec889ac56085de3a7afab5
Catz (CodingCat) <103620558+CodingCatz@users.noreply.github.com>
2026-09-09 19:34:11 +0800

    Class13:非同步行程設定

--- Assets/Scripts/Game/BigTwo/BigTwoGameController.cs
+++ Assets/Scripts/Game/BigTwo/BigTwoGameController.cs
@@ -3,6 +3,8 @@ using UnityEngine;
 using TMPro;
 using System.Collections.Generic;
 using System;
+using PokerGame.Core;
+using System.Threading.Tasks;
 
 namespace PokerGame.Game.BigTwo
 {
@@ -21,6 +23,12 @@ namespace PokerGame.Game.BigTwo
 
         #region 欄位
         /// <summary>
+        /// 遊戲的延遲時間(速度)，對外公開可調的接口
+        /// </summary>
+        [Range(0.2f, 2f)]//轉成有範圍的拉桿
+        [SerializeField]
+        private float _delayTime = 1f;
+        /// <summary>
         /// 視覺排版
         /// </summary>
         [SerializeField]
@@ -36,6 +44,10 @@ namespace PokerGame.Game.BigTwo
         /// 實作介面上的遊戲名稱
         /// </summary>
         public override string GameName => "Big Two";
+        /// <summary>
+        /// 遊戲速率(毫秒延遲)
+        /// </summary>
+        public int GameSpeed => (int)(_delayTime * 1000);
         #endregion 公開屬性
 
         #region 生命週期
@@ -93,8 +105,28 @@ namespace PokerGame.Game.BigTwo
         {
             _dealer.BeginRound();
             ClearHands();
-            //各發13張到4家
+            StartDeal(GameSpeed);
+        }
 
+        /// <summary>
+        /// [非同步行程]開局的發牌行程
+        /// </summary>
+        /// <param name="msec">遊戲速度</param>
+        private async Task StartDeal(int msec)
+        {
+            //各發13張到4家(※有可能不足4，以實際的hands數量為主)
+            for (int round = 0; round < 13; round++)
+            {
+                for (int playerIndex = 0; playerIndex < _hands.Count; playerIndex++)
+                {
+                    //視覺實體+資料分配：各玩家
+                    PlayingCard card = _dealer.DealTo(_playerLayouts[playerIndex].Root);
+                    _hands[playerIndex].Add(card);
+                    //狀態更新
+                    _playerLayouts[playerIndex].Refresh();
+                    await Task.Delay(msec);//等待：延遲任務
+                }
+            }
         }
         #endregion 私有方法
     }
